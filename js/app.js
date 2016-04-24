8c226622583a6e42fa733e9a96a7c1c2b1e03c75
John <jois.alvanez@gmail.com>
2016-04-24 16:14:33 -0500

    Menu adaptado para celu tablet con jq

--- js/app.js
+++ js/app.js
@@ -40,3 +40,41 @@ $("#galeria li a").click(function(event){
 $overlay.click(function(){
 	$overlay.hide();
 });
+
+/***********Menu Para Moviles***************/
+//Problema: El menu no funciona tan bien para moviles.
+//Solucion: Cambiar por una navegacion lateral mas util
+
+//Creemos el menu lateral y aguemoslo al menu (nav)
+var $select = $("<select></select>");
+$("#menu").append($select);
+
+//Recorrer el menu
+$("#menu a").each(function(){
+	var $anchor = $(this);
+	//Crear una opcion para el select por cada elemento del menu
+	var $option = $("<option></option>");
+	//Optener cada valor de la opcion del attributo href
+	$option.val($anchor.attr("href"));
+	//Obtener el text de cada opcion
+	$option.text($anchor.text());
+	//Agregar la opcion al select	
+	$select.append($option);
+
+	//Agregar la clase selected a cada opcion que indique la pagina actual
+	//Obtener si el link es la pagina actual (tiene clase selected)
+	if($(this).hasClass("selected"))
+	{
+		$option.prop("selected", true);
+	}
+});
+
+//Queremos que se cambie la pagina al cambiar el elemento seleccionado
+//Necesitamos usar el evento change
+$select.change(function(){
+	window.location = $select.val(); //Toma el valor de la opcion seleccionada
+});
+
+
+//Modificar CSS para esconder los links en resoluciones pequenas (width). Mostrar el Select y los botones.
+//CSS debe esconder el select en resoluciones pequenas (width)
